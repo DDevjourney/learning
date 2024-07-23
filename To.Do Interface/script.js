8fb9e96f3431df5538cf626663de6748b7f25c18
DDevjourney <123167384+DDevjourney@users.noreply.github.com>
2024-07-23 18:12:20 +0200

    V-0.02
    My brain hurts

--- To.Do Interface/script.js
+++ To.Do Interface/script.js
@@ -5,16 +5,16 @@ while (true) {
     input = prompt("What do you want to do?");
     if (input === "new") {
         let askNew = prompt("Enter a new task");
-        { listToDo.unshift(askNew) }
-    } for (let i = 0; i < listToDo.length; i++) {
-        console.log(`${listToDo[i]} added to the list.`)
+        { listToDo.unshift(askNew); }
+    } {
+        console.log(`${askNew} added to the list.`)
     }
-    if (input === "quit") { break; }
+    if (input === "quit") 
 }
 
 // De momento, tenemos el loop eterno de qué queremos hacer, para que siempre aparezca el pop up.
 // También tenemos la función de añadir nuevas tareas a un array y en el momento que se añade,
-// la consola diga lo que se ha añadido.
+// la consola diga lo que se ha añadido. Aún así, aparecen los mensajes duplicados.
 // Así como la palabra "quit" para salir de bucle.
 // Queda por hacer: deletear tareas del array, hacer que aparezca el array completo indexado
 
