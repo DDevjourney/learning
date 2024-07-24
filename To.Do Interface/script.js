e8064da50a9ce90610d1029baf4d6d07261181b2
DDevjourney <123167384+DDevjourney@users.noreply.github.com>
2024-07-24 13:44:20 +0200

    V-0.07
    Final

--- To.Do Interface/script.js
+++ To.Do Interface/script.js
@@ -1,19 +1,58 @@
-let listToDo = []
+let listToDo = [];
 let input;
 
 while (true) {
-    input = prompt("What do you want to do?");
+    input = prompt("What do you want to do? (new, list, delete, quit)");
+
     if (input === "new") {
         let askNew = prompt("Enter a new task");
-        { listToDo.unshift(askNew) }
-    } for (let i = 0; i < listToDo.length; i++) {
-        console.log(`${listToDo[i]} added to the list.`)
+        if (askNew) {
+            listToDo.push(askNew);
+            console.log(`"${askNew}" added to the list.`);
+        } else {
+            console.log("No task entered.");
+        }
     }
-    if (input === "quit") { break; }
+    else if (input === "list") {
+        console.log("Current list of tasks:");
+        if (listToDo.length === 0) {
+            console.log("No tasks in the list.");
+        } else {
+            for (let i = 0; i < listToDo.length; i++) {
+                console.log(`${i + 1}: ${listToDo[i]}`);
+            }
+        }
+    }
+    else if (input === "delete") {
+        let indexToDelete = prompt("Enter the number of the task to delete");
+        if (indexToDelete && !isNaN(indexToDelete) && indexToDelete > 0 && indexToDelete <= listToDo.length) {
+            const deletedTask = listToDo.splice(indexToDelete - 1, 1);
+            console.log(`"${deletedTask}" deleted from the list.`);
+        } else {
+            console.log("Invalid number.");
+        }
+    }
+    else if (input === "quit") {
+        console.log("Exiting the task manager.");
+        break;
+    }
+    else {
+        console.log("Unknown command. Please enter 'new', 'list', 'delete', or 'quit'.");
+    }
 }
+
+
 
 // De momento, tenemos el loop eterno de qué queremos hacer, para que siempre aparezca el pop up.
 // También tenemos la función de añadir nuevas tareas a un array y en el momento que se añade,
-// la consola diga lo que se ha añadido. Pero se repite todo.
+// la consola diga lo que se ha añadido. Pero aparecen repetidos los elementos.
 // Así como la palabra "quit" para salir de bucle.
 // Queda por hacer: deletear tareas del array, hacer que aparezca el array completo indexado
+
+
+
+
+
+
+
+
